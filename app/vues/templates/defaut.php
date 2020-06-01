5eea679e28fb0b237f931bb6ff2318a22ba8b171
romainIEPS <romain.garcia@hotmail.com>
2020-06-01 15:38:54 +0200

    Décomposition en partials + zones dynamiques
    Décomposition du template de l'application en partials + mise en place des zones dynamiques

--- app/vues/templates/defaut.php
+++ app/vues/templates/defaut.php
@@ -3,3 +3,28 @@
   ./app/vues/templates/defaut.php
   TEMPLATE DE L'APPLICATION
 */
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include_once '../app/vues/templates/partials/head.php'; ?>
+  </head>
+  <body>
+
+	  <?php include_once '../app/vues/templates/partials/nav.php'; ?>
+    <!-- END nav -->
+    <?php include_once '../app/vues/templates/partials/hero.php'; ?>
+
+    <?php include_once '../app/vues/templates/partials/section.php'; ?>
+    <!-- .section -->
+
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+      <?php include_once '../app/vues/templates/partials/footer.php'; ?>
+    </footer>
+
+  <!-- loader -->
+  <?php include_once '../app/vues/templates/partials/loader.php'; ?>
+
+  <?php include_once '../app/vues/templates/partials/scripts.php'; ?>
+  </body>
+</html>
